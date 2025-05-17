b294cf20bb3caa8aaacd84660a0b1a03945eb9cd
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-05-17 21:53:22 +0530

    Added desctiption and package info

--- mix.exs
+++ mix.exs
@@ -1,13 +1,30 @@
 defmodule FinanceEquations.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/iamkanishka/financeeqex"
+  @version "0.1.0"
+
   def project do
     [
       app: :finance_equations,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description:
+        "A collection of essential financial formulas and models implemented in Elixir. Covers topics such as interest calculations, present and future value, annuities, loan amortization, investment returns, and risk metrics",
+      package: package()
+    ]
+  end
+
+  defp package do
+    [
+      name: "finance_equations",
+      # License, e.g., MIT, Apache 2.0
+      licenses: ["Apache-2.0"],
+      links: %{
+        GitHub: @source_url
+      },
+      maintainers: ["Kanishka Naik"]
     ]
   end
 
@@ -21,6 +38,8 @@ defmodule FinanceEquations.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
